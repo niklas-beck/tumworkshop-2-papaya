c6cbcd73fab391ed52b503e1a0812a2d198a2673
Youssefkila <72973215+youssefkilani@users.noreply.github.com>
2023-12-11 19:00:35 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -136,7 +136,8 @@ resource "azurerm_storage_blob" "criticalblob" {
 // https://learn.microsoft.com/en-us/azure/role-based-access-control/built-in-roles#storage-blob-data-owner
 // Provides full access to Azure Storage blob containers and data
 resource "azurerm_role_assignment" "functionToStorage" {
-  scope                = azurerm_storage_account.storage_account.id
+  scope                = azurerm_storage_container.containerpub.resource_manager_id
+
   role_definition_name = "Storage Blob Data Owner"
   principal_id         = azurerm_linux_function_app.fxn.identity[0].principal_id
 }
